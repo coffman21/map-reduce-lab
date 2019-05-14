19e295bb6eadcbf328c2e97d4f6cedf84421adc9
Student <Student@PC-3>
2019-05-14 12:47:32 +0300

    'wit'

--- src/main/java/com/banditos/dummymapreduce/MainGenerator.java
+++ src/main/java/com/banditos/dummymapreduce/MainGenerator.java
@@ -1,6 +1,8 @@
 package com.banditos.dummymapreduce;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,7 +11,23 @@ import java.nio.file.StandardOpenOption;
 public class MainGenerator {
 
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("output/generated.txt");
+        String pathToFile = "output/generated.txt";
+        Path path = Paths.get(pathToFile);
+
+        try {
+            FileOutputStream out = new FileOutputStream(pathToFile, true);
+            FileChannel channel = out.getChannel();
+            // db query
+            while(iterate resultset) {
+                // get row result
+                writeData(channel, data);
+            }
+        } catch(Exception e){
+            //log
+        } finally {
+            channel.close();
+            out .close();
+        }
 
         for (int i = 0; i < 20; i++) {
             String s = Generator.generateWord();
